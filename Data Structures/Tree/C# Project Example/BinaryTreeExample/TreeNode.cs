4cd87b97331b775976de656bedc412274e242c9c
EstebanChavez98 <esteban.ch.v95@gmail.com>
2019-06-14 14:12:55 -0600

    Start of C# Binary Tree project and modifications with Java Version

---
+++ Data Structures/Tree/C# Project Example/BinaryTreeExample/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeExample
+{
+    public class TreeNode
+    {
+        int data;
+        String name;
+        TreeNode LeftSon, RightSon;
+        public TreeNode(int d, String nam)
+        {
+            this.data = d;
+            this.name = nam;
+            this.LeftSon = null;
+            this.RightSon = null;
+        }
+        public String toString()
+        {
+            return name + ", and it's number is " + data;
+        }
+    }
+}
