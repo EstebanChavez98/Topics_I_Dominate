c4b96da4953061cdd1b44886f033fae1d3dba6ea
Esteban Chavez Verdin <esteban.chavez@conexperti.com.mx>
2019-06-26 18:59:50 -0600

    API Agregar-Modificar

---
+++ Proyecto/APICrud/APICrudCeuta/APICrudCeuta/Data/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICrudCeuta.Data
+{
+    public class Conexion
+    {
+        public static string strIP = "DESKTOP-3IG31E1\\SQLEXPRESS", strBD = "CeutaIP";
+
+        public string conexionDESK = "Data Source=" + strIP + ";Initial Catalog=" + strBD + ";Integrated Security=true;";
+    }
+}
---
+++ Proyecto/Query Database.sql
@@ -0,0 +1,35 @@
+create table Usuarios(
+id int primary key identity(1,1) not null,
+Nombre varchar(200) not null,
+Departamento varchar(200) not null,
+Persona varchar(200) not null,
+IP varchar(20)
+)
+
+go
+create proc sp_agregarUsuario @nom varchar(200), @depto varchar(200), @persona varchar(200), @ip varchar(20)
+as
+begin
+	if (@ip != (select IP from Usuarios where IP = @ip)) or (select count(*) from Usuarios) = 0
+		insert into Usuarios values(@nom, @depto, @persona, @ip)
+end
+go
+
+
+alter proc sp_modificarIP @nom varchar(200), @ip varchar(20)
+as
+begin
+	if (select count(*) from Usuarios) != 0
+		if @nom = (select Nombre from Usuarios where Nombre = @nom)
+		begin
+			declare @ipCompare varchar(20)
+			select @ipCompare = IP from Usuarios where IP = @ip
+			if(@ipCompare is null)
+				update Usuarios set ip = @ip where Nombre = @nom
+		end
+end
+go
+
+select * from Usuarios
+
+drop table Usuarios
