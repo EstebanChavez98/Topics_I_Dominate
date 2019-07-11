4f3a0cd97bae02c96c31e566e788e2f8f0403896
Esteban Chavez Verdin <esteban.chavez@conexperti.com.mx>
2019-07-10 18:55:43 -0600

    Avance en Front End

--- FrontEndCeuta2/script.js
+++ FrontEndCeuta2/script.js
@@ -1,29 +1,27 @@
 const RESOURCE_NAME = 'https://localhost:44368/api/values';
 
+Vue.component('modal', {
+    template: '#modal-template'
+})
+
 const app = new Vue({
     el: '#app',
+    name: 'modal',
     data: {
         name: '',
         dep: '',
         person: '',
-        ip: ''
+        ip: '',
+        showModal: false,
+        info: ''
     },
     methods:{
         crearVisita() {
             axios.get(`${RESOURCE_NAME}/${this.name}&${this.dep}&${this.person}&${this.ip}&`)
-            .then(response => console.log(response.data), )
+            .then(response => this.info = response.data )
+        },
+        close() {
+            this.$emit('close');
         }
     }
 });
-
-Vue.component('modal', {
-    template: '#modal-template'
-  })
-  
-  // start app
-  new Vue({
-    el: '#app',
-    data: {
-      showModal: false
-    }
-  })
